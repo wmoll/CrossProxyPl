b223532db0266bec60f48c1e85116fc5bdd3ec09
wmoll <vladimir.mally@gmail.com>
2017-03-13 17:07:09 -0700

    Initial commit

---
+++ proxy.pl
@@ -0,0 +1,58 @@
+use strict;
+use warnings;
+
+use LWP::UserAgent;
+use HTML::Entities;
+use JSON;
+
+my $ua = new LWP::UserAgent;
+
+my $waketime;
+my $json;
+{
+  local $/;
+  open my $fh, "<", "config.txt";
+  $json = <$fh>;
+  close $fh;
+}
+my $config = decode_json($json);
+
+my $sleep_duration = $config->{'period'};
+
+while (1) {
+	$waketime = time + $sleep_duration;
+    
+	my $response = $ua->get($config->{'from'});
+	unless ($response->is_success) {
+		die $response->status_line;
+	}
+	my $content = $response->decoded_content();
+	if (utf8::is_utf8($content)) {
+		binmode STDOUT,':utf8';
+	} else {
+		binmode STDOUT,':raw';
+	}
+	
+	my $contentPath = encode_entities($content);
+
+	my $uri = $config->{'to'};
+	my $json = '{"username":"'.$config->{'username'}.'","accesskey":"'.$config->{'accesskey'}.'","label":"'.$config->{'label'}.'","content":"'.$contentPath.'"}';
+	my $req = HTTP::Request->new( 'POST', $uri );
+	$req->header( 'Content-Type' => 'application/json' );
+	$req->content( $json );
+
+	my $lwp = LWP::UserAgent->new;
+	my $response = $lwp->request( $req );
+	
+	print $response->as_string();
+    
+    sleep($waketime-time);
+}
+
+	
+
+
+
+	
+        
+
